566af11c81460b7a8b04f8fab78d5f7dbf8b3ccf
NnidsS <91644020+NnidsS@users.noreply.github.com>
2024-09-09 13:58:18 -0300

    Update criptoarquivo.py

--- Código/criptoarquivo.py
+++ Código/criptoarquivo.py
@@ -1,11 +1,11 @@
 def ler_chave_publica():
     arq = open("chavePublica.txt", "r")
-    texto = arq.read
+    texto = arq.read()
     e = ""
     n = ""
     var = True
     for numero in texto:
-        if var == True:
+        if var:
             e += numero
             if numero == ";":
                 var = False
@@ -13,48 +13,40 @@ def ler_chave_publica():
             n += numero      
     e = int(e)
     n = int(n)
-    return (e,n)
+    return (e, n)
 
 def ler_chave_privada():
     arq = open("chavePublica.txt", "r")
-    texto = arq.read
-    e = ""
+    texto = arq.read()
+    d = ""
     n = ""
     var = True
     for numero in texto:
-        if var == True:
-            e += numero
+        if var:
+            d += numero
             if numero == ";":
                 var = False
         else:
             n += numero      
     d = int(d)
     n = int(n)
-    return (d,n)
+    return (d, n)
 
 def criptografa(dicionario):
+    pass  # Função ainda não implementada
 
-    chaves_dic = dicionario.keys()
-    
 def descriptografa_para_dic(arquivo):
     num1 = ler_chave_privada()[0]
     num2 = ler_chave_privada()[1]
-    elemento_final = ""
     arq = open(arquivo, "r")
     texto = arq.read()
     
     for elemento in texto:
         letra = chr((elemento**num2) % num1)
-        elemento_final += letra
-
-
-
-
-
-
+        
 
 def escreve_no_arquivo():
- x+1   
+    x = 1  
 
 
 
